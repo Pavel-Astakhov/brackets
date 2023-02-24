bc20d5bb41d98de852d0f71fd1f4c4bea586b163
Pavel Astakhov <pastakhov88@gmail.com>
2023-02-24 11:38:40 +0300

    init: implement method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      const openBracket = bracketsConfig[j][0];
+      const closeBracket = bracketsConfig[j][1];
+
+      if(current === closeBracket){
+        if(stack[stack.length - 1] === openBracket){
+          stack.pop();
+        }
+        else{
+          stack.push(current);
+        }
+      } else if(current === openBracket){
+          stack.push(current);
+        }
+    }
+  }
+
+  return stack.length === 0;
 }
